c1b75749e4b15535bfe2e056a2b55cc161a0054a
Kimleng <kimlenghor2006@gmail.com>
2023-03-12 21:26:51 -0500

    first commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Kimleng Hor on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
